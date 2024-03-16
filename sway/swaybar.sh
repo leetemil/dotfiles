a3cc2e5fef52c78968618dd9b15fa265cc693745
Emil Petersen <leetemil@users.noreply.github.com>
2024-03-16 15:08:54 +0100

    Update swaybar.sh

--- sway/swaybar.sh
+++ sway/swaybar.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 # Date and time
 date=$(date "+%Y/%m/%d")
 formatted_date_and_week=$(date "+%Y, %B %d, Week %V, %A")
@@ -7,7 +9,7 @@ current_time=$(date "+%H:%M:%S")
 battery=$(acpi | cut -d $'\n' -f1 | cut -d " " -f3-)
 battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
-if [ $battery_status == "Discharging" ]
+if [ "$battery_status" == "Discharging" ]
 then
   battery_pluggedin='🔋'
 else
@@ -16,26 +18,26 @@ fi
 
 # Volume
 volume_and_muted=$(amixer -D pulse get Master | awk -F '[[|]|%]' 'NR==6 { print $2, $4 }' | sed 's/]//g')
-volume=$(echo $volume_and_muted | cut -d ' ' -f 1)
-sound=$(echo $volume_and_muted | cut -d ' ' -f 2)
+volume=$(echo "$volume_and_muted" | cut -d ' ' -f 1)
+sound=$(echo "$volume_and_muted" | cut -d ' ' -f 2)
 
 suffix=' %'
 prefix=' '
 
-if [ $sound = "on" ] # single '=' to compare strings
+if [ "$sound" = "on" ] # single '=' to compare strings
 then
-  if [ $volume -lt 25 ]
+  if [ "$volume" -lt 25 ]
   then
-    if [ $volume -lt 10 ]
+    if [ "$volume" -lt 10 ]
     then
       prefix='  '
     fi
     sound_icon='🔈'
-  elif [ $volume -lt 75 ]
+  elif [ "$volume" -lt 75 ]
   then
     sound_icon='🔉'
   else
-    if [ $volume -eq 100 ]
+    if [ "$volume" -eq 100 ]
     then
       prefix=''
     fi
@@ -51,17 +53,16 @@ fi
 
 # Network
 network=$(nmcli dev | grep -w "connected" | xargs)
-device=$(echo $network | cut -d ' ' -f1)
 type=$(echo $network | cut -d ' ' -f2)
 state=$(echo $network | cut -d ' ' -f3)
 connection=$(echo $network | cut -d ' ' -f4)
-signal=$(cat /proc/net/wireless | tail -n1 | awk -F "[ |.]" '{ print int($5 / 0.7) " %" }')
+signal=$(tail -n1 < /proc/net/wireless | awk -F "[ |.]" '{ print int($5 / 0.7) " %" }')
 
-if [ $state == "connected" ]
+if [ "$state" == "connected" ]
 then
   net_icon="1"
 
-  if [ $type == "wifi" ]
+  if [ "$type" == "wifi" ]
   then
     net_icon='📶'
     network_status="$connection: $signal"
